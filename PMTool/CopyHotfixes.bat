4fe39938b52965d91ce6e592e5d2be07015c8409
Troutt025 <troutt025@gmail.com>
2022-09-28 13:08:15 -0500

    Automated Location
    Changed robocopy from set drive to check first if drive exists, then copy.

--- PMTool/CopyHotfixes.bat
+++ PMTool/CopyHotfixes.bat
@@ -1,21 +1,41 @@
 @ECHO off
 REM
 REM Title:  Copy Hotfixes
-REM Author: Jesse Troutt
-REM Date:   4-28-2022
+REM Date:   9/28/2022
 REM
 REM README: This file is not to be changed. To add or remove machines, please edit the Servers.txt and Workstations.txt files.
 REM         
-REM         
+REM 
 
+REM Variable for Hotfix location
+set hotfixLocation=d
+ 
 ECHO Server Hotfixes
 ECHO:
-for /f %%i in (D:\PMTool\Machines\Servers.txt) do robocopy D:\PMTool\Hotfixes\Server\ \\%%i\D$\Hotfixes\ /MIR /S /E /COPYALL /R:1 /W:5 /NFL /NS /NJS
+for /f %%i in (Machines\Servers.txt) do (
+	IF EXIST \\%%i\D$\ (
+		mkdir D:\Hotfixes
+		set hotfixLocation=d
+	) ELSE (
+		mkdir C:\Hotfixes
+		set hotfixLocation=c
+	)
+	robocopy Hotfixes\Server\ \\%%i\%hotfixLocation%\Hotfixes\ /MIR /S /E /COPYALL /R:1 /W:5 /NFL /NS /NJS
+)
 
 ECHO:
 ECHO Workstation Hotfixes
 ECHO:
-for /f %%i in (D:\PMTool\Machines\Workstations.txt) do robocopy D:\PMTool\Hotfixes\Workstation\ \\%%i\D$\Hotfixes\ /MIR /S /E /COPYALL /R:1 /W:5 /NFL /NS /NJS
+for /f %%i in (Machines\Workstations.txt) do (
+	IF EXIST \\%%i\D$\ (
+		mkdir D:\Hotfixes
+		set hotfixLocation=d
+	) ELSE (
+		mkdir C:\Hotfixes
+		set hotfixLocation=c
+	)
+	robocopy Hotfixes\Workstation\ \\%%i\%hotfixLocation%\Hotfixes\ /MIR /S /E /COPYALL /R:1 /W:5 /NFL /NS /NJS
+)
 
 ECHO:
 pause
